73a97066d8fb1f0e76234031d8924aa63c01ea1c
Rufai Mustapha <etop94@gmail.com>
2021-05-21 22:14:57 +0100

    fix netlify build
    netlify is treating warning as errors. I removed the logo.svg on the App.js component because it wasn't in use.

--- moolifrontend/src/App.js
+++ moolifrontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Card from './Component/Card'
 
@@ -10,22 +10,6 @@ function App() {
       <Card />
       <Card />
     </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
